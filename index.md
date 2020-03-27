26e024376621480768e78c6b5a6d52bec3c79671
mogs.git <mogs.mail1@gmail.com>
2020-03-27 21:13:25 +0000

    please go to the website

--- index.md
+++
@@ -1,2 +0,0 @@
-Hello
-[Link to calculator](/calculator/)
---
+++ script.js
@@ -0,0 +1,45 @@
+let projects = ["Calculator", "Perlin vines", "egg", "greg", "greg", "greg"];
+let links = ["http://mogs-git.github.io//calculator/", "http://mogs-git.github.io//perlin_vine/"];
+let background_colours = ["hsl(101, 61%, 25%)", "hsl(0, 61%, 30%)",  "rgb(73, 156, 74)","rgb(73, 156, 74)", "rgb(73, 156, 74)", "rgb(73, 156, 74)"];
+
+const welcome_container = document.getElementById("welcome_container");
+const projects_container = document.getElementById("projects_container");
+
+let desc_node = document.createElement("p");
+desc_node.classList.add("desc");
+welcome_container.appendChild(desc_node);
+
+function brighten_hsl_string(s) {
+	s = s.split("");
+	let n = s[s.length-4];
+	n = Number(n) + 3;
+	s[s.length-4] = String(n);
+	return s.join("");
+} 
+
+for (let i = 0; i < projects.length; i++) {
+	let item = document.createElement("div");
+	item.textContent = projects[i];
+	item.style.backgroundColor = background_colours[i];
+	item.classList.add("project_item");
+	projects_container.appendChild(item);
+	item.addEventListener("mouseover", (e) => {
+		e.target.style.backgroundColor = brighten_hsl_string(background_colours[i]);
+		desc_node.textContent = descriptions[i];	
+
+		desc_node.classList.toggle("fade");
+	})
+	item.addEventListener("mouseout", (e) => {
+		e.target.style.backgroundColor = background_colours[i];	
+		desc_node.classList.toggle("fade");
+	})
+	item.addEventListener("click", () => {
+		window.open(links[i], '_blank');
+	})
+
+}
+
+let descriptions = [
+"A simple calculator built using HTML, CSS and vanilla JS.",
+"A generative art project built using p5.js."
+]
